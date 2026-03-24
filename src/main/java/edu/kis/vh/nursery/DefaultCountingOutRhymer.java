56ed93490ae7fbcd63811e6b7abfb86cd9aa0631
Student258491 <258491@edu.p.lodz.pl>
2026-03-24 21:43:05 +0100

    7. dodano "final" do atrybutów

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -5,7 +5,7 @@ public class DefaultCountingOutRhymer {
     public static final int MAX_SIZE = 12;
     public static final int EMPTY_RHYMER_INDICATOR = -1;
     public static final int DEFAUL_VALUE = -1;
-    private int[] numbers = new int[MAX_SIZE];
+    private final int[] numbers = new int[MAX_SIZE];
 
     public int getTotal() {
         return total;
